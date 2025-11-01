ef5e946a1f7a4d90080220c31b1b2e38a78c548a
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2025-11-01 13:55:44 +0100

    advanced merge_sort & quick_sort

--- cap3_Alghs_Ordinamento_&_Ricerca.py
+++ cap3_Alghs_Ordinamento_&_Ricerca.py
@@ -1,4 +1,11 @@
 
+#STABILE: mantiene l’ordine relativo degli elementi uguali.
+#IN-PLACE: ordina senza usare memoria aggiuntiva (temps data structures/ect) proporzionale alla dimensione dell’input
+
+#RICORSIONE: 
+#ITERAZIONE: 
+
+
 def bubble_sort(elements):  #BUBBLE SORT
     last_elem_index = len(elements)-1
     for pass_no in range(last_elem_index, 0, -1):
@@ -56,19 +63,15 @@ def insertion_sort(elements):  #INSERTION SORT
     return elements   #classico return finale per dare la risposta ultimata
 
 
-def merge_sort(elements):  #MERGE SORT, divide et impera! 🔥 (anche nei casi peggiori è stabile, ideale x grandi datasets)
+def merge_sort(elements):  #divide et impera w ricorsione. O(n log n), non in-place, stabile, usa ricorsione.
     if len(elements) <= 1:
         return elements
     mid = len(elements) // 2  #// is divisione intera e.g. 5//2 =2, quindi se ci sono 5 elems, 2 andranno in left e 3 in right
       #NON USARE (0+(len(elems)-1))//2 !!quello va bene solo x binary_search , altrimenti qua elems=2 sarebbe mid=0 quindi con left e right fanno ricorsione infinita error StackOverflow!
-    left = elements[:mid]   #create new list
-    right = elements[mid:]  #create new list
-
-    merge_sort(left)  #non continuare il codice, finche non ti viene restituito qualcosa dall'interno. stessa cosa per tutti i figli interni.
-    merge_sort(right)
+    left = merge_sort(elements[:mid])  #non continuare il codice, finche non ti viene restituito qualcosa dall'interno del merge_sort!!. stessa cosa per tutti i figli interni.
+    right = merge_sort(elements[mid:])
     #-- first cycle here arrive WHEN elements = 2items, merge_sort(left)->ha eseguito merge_sort(with 1 item) e gli è stato restituito, same for merge_sort(right)
     #-- second cycle here arrive WHEN ritornano i 2 segmenti piu piccoli possibili (2elements ora ordinati)-(2elements ora ordinati) or (2elements ora ordinati)-(1elements)
-
     a,b,c = 0,0,0  #a x scandire left, b x scandire right, c x index in list Elements
     while a<len(left) and b<len(right):  #gira solo fino a quando o A o B hanno scandito tutti i loro items nella loro lista
         if left[a] < right[b]:
@@ -78,7 +81,6 @@ def merge_sort(elements):  #MERGE SORT, divide et impera! 🔥 (anche nei casi p
             elements[c] = right[b]
             b += 1
         c += 1  #first cycle avanza di 1 index su lista originale
-
     while a<len(left):  #se ci sono altri elementi nella meta sx
         elements[c] = left[a]
         a += 1
@@ -89,6 +91,40 @@ def merge_sort(elements):  #MERGE SORT, divide et impera! 🔥 (anche nei casi p
         c += 1
     return elements  #fist cycle here elements=2items, ora questi 2items sono ordinati
     #ora si confronterà con i risultati dell'altro ramo (che anche lui aveva elements=2 oppure elements=1 )
+
+
+#🔥🔥ADVANCED MERGE_SORT: iterative divide et impera direction bottom->up.
+#O(n log n), in-place(quasi), stabile, usa iterazione anziche ricorsione (evita cosi molte calls), ideale per grandi dataset.
+def merge_sort_iterative(elements):
+    n = len(elements)
+    size = 1  # dimensione iniziale dei blocchi da fondere (1 elemento)
+    # funzione helper per fondere due sottoliste ordinate
+    def merge(left, right):
+        merged = []
+        a = b = 0
+        while a < len(left) and b < len(right):
+            # <= per mantenere la stabilità (mantiene ordine originale se uguali)
+            if left[a] <= right[b]:
+                merged.append(left[a])
+                a += 1
+            else:
+                merged.append(right[b])
+                b += 1
+        # aggiungi eventuali elementi rimasti
+        merged.extend(left[a:])
+        merged.extend(right[b:])
+        return merged
+    # ciclo principale: aumenta progressivamente la dimensione dei blocchi
+    while size < n:
+        for start in range(0, n, size * 2):
+            mid = min(start + size, n)        # inizio della seconda metà
+            end = min(start + size * 2, n)    # fine della seconda metà
+            left = elements[start:mid]
+            right = elements[mid:end]
+            elements[start:end] = merge(left, right)
+        size *= 2  # raddoppia la dimensione dei blocchi
+    return elements
+
 
 def shell_sort(elements):   #[5, 2, 6, 1, 3] ,  anche se è migliore di Insertion, Bubble o Selection Sort, NON supera MergeSort, TimSort o QuickSort ed è anche non stabile(non garantisce di mantenere l'ordine tra elementi equivalenti)
     distance = len(elements) //2  #first cycle 5//2 =2
@@ -107,8 +143,9 @@ def shell_sort(elements):   #[5, 2, 6, 1, 3] ,  anche se è migliore di Insertio
     return elements
 
 
-def quick_sort(elements):  #🔥 O(n^2)(ma normalmente è O(n log n)), è stabile, è veloce(in media), su array enormi puo dare err RecursionError. [5, 2, 6, 1, 3] 
+def quick_sort(elements):  #O(n^2)(ma normalmente è O(n log n)), non-stabile, è veloce(in media), su array enormi puo dare err RecursionError. [5, 2, 6, 1, 3] 
       #here è non-inplace per didattica, ma la versione reale non crea nuove liste temps ma sposta gli elements
+      #!here il pivot non è scelto a random, quindi here la partizione dipende completamente da come è ordinato l’input(se è ordinato/quasi ordinato, il pivot “middle” non divide bene i dati!).  
     if len(elements) <= 1:
         return elements
     pivot = elements[ len(elements) // 2 ]   # fc pivot = elements[1] #qua in realta ci serve solo un valore random della lista, non necessariamente il medio. dunque si puo fare anche pivot=random.choice(elements)
@@ -127,57 +164,40 @@ def quick_sort(elements):  #🔥 O(n^2)(ma normalmente è O(n log n)), è stabil
     return quick_sort(left) + middle + quick_sort(right)  #concatena i pezzi ordinandoli, e passa il result all'annidamento superiore
 
 
-def partition3_Xquicksortrandom(A, l, r):
-    """
-    3-way partitioning
-    - A[l..r] is partitioned into:
-        A[l..m1-1] < pivot
-        A[m1..m2] == pivot
-        A[m2+1..r] > pivot
-    Returns (m1, m2)
-    """
-    pivot = A[l]
-    m1 = l       #first index of pivot
-    m2 = l       #last index of pivot
-    for i in range(l + 1, r + 1):
-        if A[i] < pivot:
-            m1 += 1
-            A[i], A[m1] = A[m1], A[i]  # swap element < pivot to left
-            if m1 != m2:
-                m2 += 1
-                A[i], A[m2] = A[m2], A[i]  # shift equal pivot to middle
-        elif A[i] == pivot:
-            m2 += 1
-            A[i], A[m2] = A[m2], A[i]      # swap element == pivot to middle
-    A[l], A[m1] = A[m1], A[l]  # move pivot to its final place
-    return m1, m2
-def quick_sort_randomized(A, l=0, r=None):   #🔥🔥🔥 non è stabile(non garantisce di mantenere l'ordine tra elementi equivalenti)
-    """
-    QuickSort ottimizzato con:
-    -pivot random: riduce enormemente per prob di avere caso peggiore O(n**2)
-    -3-way partitioning: ricude enormemente squilibli con molti cloni ed eviti ricursioni inutili. scegli pivot x,metti a sx gli elems <x,  dx gli elems >x, al centro quelli ==x, 
-        #ottieni indexes m_1 and m_2 che delimitano il segmento ==x. 
-    -tail recursion sulla metà più grande convertita in loop: riduci profondita massima delle ricursione O(logn) (in media) 
-       #facendo ricorsione sul segmento piu small, e per il segmento big usi while().
-    #w pivot riandom+3-way partition ottieni i segmenti bilanciati(cioe che sono +- della stessa length), dunque la profondita ricorsiva è log_n
-       #(se fosse sbilanciato cioe pivot sempre min/max profondita ricorsiva +- n. quindi il caso peggiore sarebbe O(n**2))
-    """
-    if r is None:
-        r = len(A) - 1
-    while l < r:  # loop per sostituire la ricorsione sulla metà più grande
-        # scegliere un pivot casuale e metterlo all'inizio
-        k = random.randint(l, r)
-        A[l], A[k] = A[k], A[l]
-        # partizionamento in 3 parti
-        m1, m2 = partition3_Xquicksortrandom(A, l, r)
-        # ricorsione solo sulla metà più piccola
-        if (m1 - l) < (r - m2):
-            quick_sort_randomized(A, l, m1 - 1)
-            l = m2 + 1  # tail recursion sulla metà più grande
+#🔥🔥ADVANCED QUICK_SORT: Randomized QuickSort con 3-way partitioning (Dutch National Flag). 
+#O(n log n), è in-place, non-stabile, 1/1M possibilita di StackOverflow. estremamente efficente con clones, grazie a pivot random hai altissime prob che: 1/3 degli item vadano a sx, 1/3 degli item vadano a dx, 1/3 degli item vadano in centro.
+#code in java in F:\JAVA\java_ucsdCoursera_datastr&alghs\Course1_AlgorithmicToolbox\module4_DivideEtConquer\QuickSortRandomized3WayPartitioning_perfectXManyClones.java
+def main():
+    n = int(input())
+    nums = list(map(int, input().split()))  # build the staticarr nums
+    randomized_quick_sort(nums, 0, n - 1)
+    print(*nums)  # stampa in linea
+def randomized_quick_sort(arr, left, right):
+    if left >= right:
+        return
+    pivot_index = left + int(random.random() * (right - left + 1))  # genera idx random left->right(inclusi entrambi)
+    swap(arr, left, pivot_index)  # now pivot è in testa (in idx left)!
+    mid = partition3(arr, left, right)  # 3way partitioning, return arr con 2 idx: lt e gt che delimitano(inclusi) la zona item==pivot cioe ==arr[left]
+    randomized_quick_sort(arr, left, mid[0] - 1)  # ricorsione su zona <pivot
+    randomized_quick_sort(arr, mid[1] + 1, right)  # ricorsione su zona >pivot
+def partition3(arr, left, right):
+    pivot = arr[left]  # xk ricorda che dopo swap value pivot è andato in testa!!
+    lt = left   # lessthan, prima posizione dove finiranno gli elementi <pivot. zona arr[left .. lt-1]
+    gt = right  # greatherthan, ultima posizione dove finiranno gli elementi >pivot. zona arr[gt+1 .. right]
+    i = left    # x scandire the items
+    while i <= gt:  # at the start le regioni <pivot e >pivot sono vuote. tutto è nell'area non processata arr[i..gt]
+        if arr[i] < pivot:
+            swap(arr, lt, i)
+            lt += 1
+            i += 1  # swap values arr[lt]<->arr[i], after +1 su entrambi i pointers: lt++ perche ora l'item (ex arr[i]) è in zona <pivot quindi devo update zona arr[left .. lt-1]!
+        elif arr[i] > pivot:
+            swap(arr, i, gt)
+            gt -= 1  # non fare i++, perche dopo swap ora in arr[i] c'è un item sconosciuto(da processare in next cycle)!
         else:
-            quick_sort_randomized(A, m2 + 1, r)
-            r = m1 - 1  # tail recursion sulla metà più grande
-    return A
+            i += 1  # caso ==pivot: espandiamo la zona ==pivot lasciando semplicemnte l'elemento dove sta, senza spostarlo in zona a DX dove c'è zona arr[gt+1 .. right], o in zona SX
+    return [lt, gt]  # 🔥🔥in zona tra puntatori lt->gt(inclusi entrambi) ci sono tutti gli item ==pivot!!
+def swap(arr, i, j):  # swap items in idx i<->j
+    arr[i], arr[j] = arr[j], arr[i]
 
 
 def heap_sort(elements):   #O(n log n), non è stabile, è in-place, va bene anche per array molto grandi
@@ -207,13 +227,13 @@ def heap_sort(elements):   #O(n log n), non è stabile, è in-place, va bene anc
         heapify(elements, i, 0)  # sistema l'heap con la radice ridotta
     return elements
 
-
 #def intro_sort()  #🔥(quick_sort + se sta andando male passa a heap_sort), non è stabile, è in-place, è robusto
   #un problema del quick_sort è che se array è gia ordinato ed pivot scelto male, algh degrada a O(n**2)
   #intro_sort invece parte come quick_sort, ma se la profondità della ricorsione supera c log n (dunque il partizionamento sta andando male),
     #passa a heap_sort (che è un O(nlogn))(e rimane anche in-place!)
 
 
+#🔥🔥
 #def tim_sort(elements, run=2):   #🔥(standart in enterprise & standart predefinito in molti linguaggi (e.g.JAVA), è stabile, è adattivo, O(n log n) nel caso peggiore, è ottimo su dati del real world(dove cmnq i dati arrivano gia ordinati/parzialmente ordinati)) 
    #combina insertion_sort (per creare dei segmenti gia ordinati) + merge_sort   #here run piccolo per esempio, 
 def insertion_sort_TIM(arr, left, right):
@@ -299,7 +319,7 @@ def binary_search(elements,item):  #🔥 #[2,3,4,5,6][3]  per funzionare gli ele
     first = 0
     last = len(elements)-1  #fc  =4     #importante -1 !!
     isFound = False
-    while first <=last and not isFound:  #!importante <= !!
+    while first <=last and not isFound:  #!importante <= !!!
         midpoint = (first+last) //2  #divisione intera
           #mid=first+(last-first)/2;  in c++/java questo è pro per evitare stackoverflow se first e last sono davvero bigs
         if elements[midpoint] == item:
